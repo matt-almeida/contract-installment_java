c3a0c1bd1862d67b77f65da5c774ca0f704b1b46
Matheus <matheusalmeidad20@gmail.com>
2025-07-08 20:46:56 -0300

    Modifying the process in Program in the application package

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,12 @@
 package application;
 
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,6 +15,33 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato:");
+
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int numberInstallments = sc.nextInt();
+
+        PaypalService paypalService = new PaypalService();
+        ContractService contractService = new ContractService(paypalService);
+
+        contractService.processContract(contract, numberInstallments);
+
+        System.out.println("Parcelas:");
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
 
         sc.close();
     }
