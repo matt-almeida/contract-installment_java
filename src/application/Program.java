255534ff9262948f0ac76043af38a0edea33e9fb
Matheus <matheusalmeidad20@gmail.com>
2025-07-08 20:41:51 -0300

    Adding OnlinePaymentService interface in services

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
+
 
         sc.close();
     }
